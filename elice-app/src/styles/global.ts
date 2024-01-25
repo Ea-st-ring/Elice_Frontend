4e474a6762fbc1b0aadeb404a5447c1b6cf61786
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2024-01-26 01:31:11 +0900

    [style] 폰트 추가

--- elice-app/src/styles/global.ts
+++ elice-app/src/styles/global.ts
@@ -2,14 +2,18 @@ import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
   html {
-    scroll-behavior: smooth;
+    scroll-behavior: smooth;    
+  }
+  * {
+    box-sizing: border-box;
+    font-family: 'Pretendard';
   }
   body {
-    box-sizing: border-box;
-    margin: 0px;
+    margin: 0;
     display: flex;
     justify-content: center;
     align-items: center;
+    background-color: #fff;
   }
   th,
   td {
@@ -26,4 +30,10 @@ export const GlobalStyle = createGlobalStyle`
     appearance: none;
     cursor: pointer;
   }
+  @font-face {
+    font-family: 'Pretendard';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+}
 `
