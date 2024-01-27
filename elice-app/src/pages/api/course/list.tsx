6d78f87856ad0dbd0e1bed7b70f766f6bc7a9180
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2024-01-27 22:58:23 +0900

    [feat] 쿼리 수정
    offset, countPerPage 파라미터를 이용한 Data Fetching 개선

--- elice-app/src/pages/api/course/list.tsx
+++ elice-app/src/pages/api/course/list.tsx
@@ -4,40 +4,39 @@ import OrgCourseListResponses from '@/type/typings'
 export default async function List(request: any, response: any) {
   if (request.method == 'GET') {
     const db = (await connectDB).db('elice')
-    let result = await db.collection('course').find().toArray()
+    const query = JSON.parse(request.query.filter_conditions)
+    if (query.$and[0].title == '') {
+      delete query.$and[0].title
+    }
+    if (query.$and[1].$or.length == 0) {
+      delete query.$and[1].$or
+    }
 
-    let course = result.map((course) => {
+    const offset = parseInt(request.query.offset)
+    const countPerPage = parseInt(request.query.countPerPage)
+
+    const result = await db
+      .collection('course')
+      .find(query)
+      .limit(countPerPage)
+      .skip(offset)
+      .toArray()
+    const courseCount = await db.collection('course').countDocuments(query)
+    var course = result.map((course) => {
       return {
-        id: course._id,
+        id: course._id.toString(),
         title: course.title,
-        description: course.description,
         enrollType: course.enroll_type,
         isFree: course.is_free,
         shortDescription: course.short_description,
         logoFileUrl: course.logo_file_url,
       }
     })
-    // 조건에 맞지 않는 데이터 제거
-    course = course.filter((course) => {
-      return (
-        course.logoFileUrl != null &&
-        course.enrollType != 5 &&
-        course.enrollType != 1 &&
-        course.shortDescription != null
-      )
-    })
-    // 중복 데이터 제거
-    course = course.filter((course, index, self) => {
-      return (
-        index ===
-        self.findIndex((t) => {
-          return t.title === course.title
-        })
-      )
-    })
-    let courseCount = course.length
-    result = { courseCount: courseCount, courses: course } as unknown as OrgCourseListResponses
 
-    return response.status(200).json(result)
+    const data = {
+      courseCount: courseCount as OrgCourseListResponses['courseCount'],
+      courses: course as OrgCourseListResponses['courses'],
+    } as OrgCourseListResponses
+    return response.status(200).json(data)
   }
 }
