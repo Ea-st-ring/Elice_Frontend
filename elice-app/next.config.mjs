5cd6740d4b769cb8dba2d8375363a726668b23e9
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2024-01-28 00:10:43 +0900

    [chore] 리다이렉트 설정

--- elice-app/next.config.mjs
+++ elice-app/next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/courses',
+        permanent: true,
+      }
+    ]
+  },
   compiler: {
     styledComponents: {
       ssr: true,
