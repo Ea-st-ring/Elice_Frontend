e79897147a3d630afd944f2c456c63dae2c8fa2a
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2024-01-28 00:21:04 +0900

    [chore] 불필요 import 제거

--- elice-app/src/app/page.tsx
+++ elice-app/src/app/page.tsx
@@ -1,7 +1,3 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import styled from 'styled-components'
-
 export default function Home() {
-  return <div>테스트</div>
+  return <div></div>
 }
