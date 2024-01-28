332a23eb4ab347c7acc219f0cedb5716223a30e5
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2024-01-28 17:07:14 +0900

    [del] 중복 코드 삭제

--- elice-app/next.config.mjs
+++ elice-app/next.config.mjs
@@ -9,22 +9,6 @@ const nextConfig = {
       }
     ]
   },
-  compiler: {
-    styledComponents: {
-      ssr: true,
-      displayName: true, // 클래스명에 컴포넌트 이름을 붙임
-      pure: true, // dead code elimination (사용되지 않는 속성 제거)
-    },
-  },
-  reactStrictMode: false, // api 두번 호출 방지
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn-api.elice.io',
-      },
-    ],
-  },
 }
 
 export default nextConfig
